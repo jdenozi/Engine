ab96b9905849a41958b92a8c7f5b0d6efbfa94ae
Julien Denozi <j-denozi@innopsys.fr>
2024-10-04 17:29:25 +0200

    Add logger class

--- src/events/Events.cpp
+++ src/events/Events.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Events.h"
 #include "engine/Engine.h"
+#include "utility/Logger.h"
 
 Events::Events() {
 
@@ -14,6 +15,7 @@ void Events::polls() {
 void Events::init() {
     auto w = EngineCore.window();
     glfwSetWindowCloseCallback(w.get(), [](GLFWwindow *w){
+        LOG_DEBUG("Close application");
         EngineCore.exit();
     });
 }
--- src/utility/Logger.h
+++ src/utility/Logger.h
@@ -1,12 +1,46 @@
-//
-// Created by juliend@innopsys.lan on 04/10/24.
-//
+#pragma once
 
-#ifndef GAMEENGINE_LOGGER_H
-#define GAMEENGINE_LOGGER_H
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <iomanip>
+#include <mutex>
 
 class Logger {
+public:
+    enum class LogLevel {
+        INFO, DEBUG, WARNING, ERROR
+    };
 
+    static Logger &getInstance() {
+        static Logger instance;
+        return instance;
+    }
+
+    void log(LogLevel level, const std::string &message, const char *file, int line);
+
+    Logger(const Logger &) = delete;
+
+    Logger &operator=(const Logger &) = delete;
+
+private:
+
+    Logger();
+
+    ~Logger();
+
+    static std::string getCurrentTimestamp();
+
+    static std::string extractFilename(const std::string &filepath);
+
+    std::ofstream logFile_;
+    std::mutex mutex_;
 };
 
-#endif //GAMEENGINE_LOGGER_H
+static Logger &AppLogger = Logger::getInstance();
+
+#define LOG_INFO(message) AppLogger.log(Logger::LogLevel::DEBUG, message, __FILE__,  __LINE__)
+#define LOG_DEBUG(message) AppLogger.log(Logger::LogLevel::DEBUG, message, __FILE__, __LINE__)
+#define LOG_WARNING(message) AppLogger.log(Logger::LogLevel::WARNING, message, __FILE__, __LINE__)
+#define LOG_ERROR(message) AppLogger.log(Logger::LogLevel::ERROR, message, __FILE__, __LINE__)
