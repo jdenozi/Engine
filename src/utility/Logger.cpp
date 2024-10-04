c3cc5c6ea8ffb0c18efd5b19e791c57e19f59243
Julien Denozi <j-denozi@innopsys.fr>
2024-10-04 17:28:54 +0200

    Adding glad and glfw

---
+++ src/utility/Logger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by juliend@innopsys.lan on 04/10/24.
+//
+
+#include "Logger.h"
---
+++ src/utility/Logger.h
@@ -0,0 +1,12 @@
+//
+// Created by juliend@innopsys.lan on 04/10/24.
+//
+
+#ifndef GAMEENGINE_LOGGER_H
+#define GAMEENGINE_LOGGER_H
+
+class Logger {
+
+};
+
+#endif //GAMEENGINE_LOGGER_H
